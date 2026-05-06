b45112b9782a3e093344e17b6328d3eb8e9171d5
ameliep2029 <amelie.proietti2029@gbwl.org>
2026-05-06 14:22:20 +0000

    done

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log("hello world");
+// Create a boolean variable to track current mode 
+let darkMode = false; 
+
+/* NEW FUNCTION: document.querrySelector()
+let's us SELECT an HTML ELEMENT from the page to store a REFERENCE (pointer) to that element
+*/
+
+
+const body = document.querySelector("body"); 
+
+//heading is a JS VARIABLE that points to the element with the id="main-heading" (like selecting in CSS)
+const heading = document.querySelector("#main-heading");
+const description = document.querySelector("#description"); 
+const toggleBtn = document.querySelector("#toggle");
+
+
+// Can change attributes & properties using JS 
+heading.textContent = "Light & Dark Website"; // JS wrote HTML code!
+heading.style.color = "olive"; 
+heading.style.background ="pink";
